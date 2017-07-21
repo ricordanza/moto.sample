88fd4af5ebc2bb4608a146866f1a4be7befe4bcf
Ricordanza <Ricordanza@users.noreply.github.com>
2017-07-21 15:41:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ sys.path.append('./src')
 sys.path.append('./test')
 
 setup(
-	name = "moto.sample",
+    name = "moto.sample",
     version = "0.1",
     packages = find_packages(),
     test_suite = 'test_sample.suite'
