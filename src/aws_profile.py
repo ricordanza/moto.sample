35ca01b5e53667abe8015113f62d7da63bcb6a12
Toshiaki Aoike <aoike@cloudpack.jp>
2017-07-21 14:53:38 +0900

    modify aws session setting

--- src/aws_profile.py
+++ src/aws_profile.py
@@ -3,6 +3,5 @@
 from boto3.session import Session
 
 def get_client(_type):
-    #return Session().client(_type)
+    return Session().client(_type)
     #return Session(profile_name='<profile_name_here>').client(_type)
-    return Session(profile_name='dev').client(_type)
